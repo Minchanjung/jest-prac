23abeb69a53afea1080f21ca1b093910ac8f6882
Minchan Jung <minchanjung1@gmail.com>
2022-03-11 13:26:30 +0000

    capitalize.js tested and func

--- code/capitalize.js
+++ code/capitalize.js
@@ -1,5 +1,11 @@
 const capitalize = (string) => {
-    string[0].toUpperCase();
+    let myArray = string.split(" ");
+    for (let i=0; i < myArray.length; i++) {
+        myArray[i] = myArray[i].charAt(0).toUpperCase() + myArray[i].slice(1)
+    }
+    let text = myArray.toString();
+    text = text.replace(/,/g, " ");
+    return text;
 }
 
 export default capitalize;
--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,5 +1,21 @@
 import capitalize from "../code/capitalize";
 
-test("one word capitalize", () => {
-    expect(capitalize('ben')).tobe('Ben')
+test('"test" should become "Test"', () => {
+    expect(capitalize('test')).toBe('Test');
+  });
+
+test('"Test" should become "Test"', () => {
+    expect(capitalize("Test")).toBe("Test");
+});
+
+test('multi word test', () => {
+    expect(capitalize("hi my name is")).toBe("Hi My Name Is");
+});
+
+test('multi word with numbers', () => {
+    expect(capitalize("hi 2343this is a test 1222")).toBe("Hi 2343this Is A Test 1222");
+});
+
+test('handle extra space', () => {
+    expect(capitalize("    hey     hi    ")).toBe("    Hey     Hi    ")
 });
